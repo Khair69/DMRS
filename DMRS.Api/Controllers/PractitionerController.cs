a6835fe869dd793f35cb4d31974c0af0f946023d
Khair69 <khair04@outlook.com>
2026-02-15 19:52:52 +0300

    practitioner controller (search not working)

---
+++ DMRS.Api/Controllers/PractitionerController.cs
@@ -0,0 +1,14 @@
+﻿using DMRS.Api.Application.Interfaces;
+using DMRS.Api.Domain.Interfaces;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+
+namespace DMRS.Api.Controllers
+{
+    public class PractitionerController : FhirBaseController<Practitioner>
+    {
+        public PractitionerController(IFhirRepository repository, ILogger<PractitionerController> logger, FhirJsonDeserializer deserializer, IFhirValidatorService validator) : base(repository, logger, deserializer, validator)
+        {
+        }
+    }
+}
---
+++ DMRS.Api/Infrastructure/Search/PractitionerIndexer.cs
@@ -0,0 +1,65 @@
+﻿using DMRS.Api.Application.Interfaces;
+using DMRS.Api.Domain;
+using Hl7.Fhir.Model;
+
+namespace DMRS.Api.Infrastructure.Search
+{
+    public class PractitionerIndexer : ISearchIndexer
+    {
+        public List<ResourceIndex> Extract(Resource resource)
+        {
+            var indices = new List<ResourceIndex>();
+
+            if (resource is Practitioner practitioner)
+            {
+                // Index the ID
+                indices.Add(new ResourceIndex
+                {
+                    ResourceType = "Practitioner",
+                    ResourceId = practitioner.Id,
+                    SearchParamCode = "_id",
+                    Value = practitioner.Id
+                });
+
+                // Index Gender
+                if (practitioner.Gender.HasValue)
+                {
+                    indices.Add(new ResourceIndex
+                    {
+                        ResourceType = "Practitioner",
+                        ResourceId = practitioner.Id,
+                        SearchParamCode = "gender",
+                        Value = practitioner.Gender.ToString().ToLower()
+                    });
+                }
+
+                // Index Family Name (Last Name)
+                foreach (var name in practitioner.Name)
+                {
+                    if (!string.IsNullOrEmpty(name.Family))
+                    {
+                        indices.Add(new ResourceIndex
+                        {
+                            ResourceType = "Practitioner",
+                            ResourceId = practitioner.Id,
+                            SearchParamCode = "family",
+                            Value = name.Family.ToLower()
+                        });
+                    }
+                    if (!string.IsNullOrEmpty(name.Given.First()))
+                    {
+                        indices.Add(new ResourceIndex
+                        {
+                            ResourceType = "Practitioner",
+                            ResourceId = practitioner.Id,
+                            SearchParamCode = "given",
+                            Value = name.Given.First().ToLower()
+                        });
+                    }
+                }
+            }
+            return indices;
+
+        }
+    }
+}
