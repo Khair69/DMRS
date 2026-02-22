6668025d98abe1b7bd2fc04a67b4bf4642423736
Khair69 <khair04@outlook.com>
2026-02-22 17:37:58 +0300

    updated database with objects history

---
+++ DMRS.Api/Migrations/20260222143428_history.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMRS.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class history : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FhirResourceVersions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    ResourceType = table.Column<string>(type: "text", nullable: false),
+                    VersionId = table.Column<int>(type: "integer", nullable: false),
+                    LastUpdated = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    RawContent = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FhirResourceVersions", x => new { x.ResourceType, x.Id, x.VersionId });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FhirResourceVersions");
+        }
+    }
+}
